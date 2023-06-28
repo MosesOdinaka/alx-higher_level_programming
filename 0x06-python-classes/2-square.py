1ef90adc33effe2c41a76a8de0a7683d9c6a59e2
MosesOdinaka <iriele.moses@gmail.com>
2023-06-27 17:46:03 -0700

    A python class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,3 +9,6 @@ class Square:
         """Initializes the size attribute of the Square object"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
