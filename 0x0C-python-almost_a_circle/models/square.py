fd0d5ce5f8488d6dadb1eeb9461b91d664b7eeae
MosesOdinaka <iriele.moses@gmail.com>
2023-07-17 18:29:35 -0700

    Updated Class Square.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square class."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
