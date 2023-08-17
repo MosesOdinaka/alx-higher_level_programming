21af065db79e0e50a37ffbdb87db8deb88ca8e6a
MosesOdinaka <iriele.moses@gmail.com>
2023-08-17 16:57:00 -0700

    displays the privileges granted to the MySQL users 'user_0d_1' and 'u…

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- This code displays the privileges granted to the MySQL users 'user_0d_1' and 'user_0d_2' on the local server.
+-- This code displays the privileges granted to the MySQL
+-- users 'user_0d_1' and 'user_0d_2' on the local server.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
