b8c79a19a1f4fb7a749f7f708e320e5aea8e9e17
MosesOdinaka <iriele.moses@gmail.com>
2023-08-17 17:31:27 -0700

    script that lists all Comedy shows in the database hbtn_0d_tvshows.

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- This query selects the titles of TV shows in the 'Comedy'
+-- genre and orders them by title.
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
