a3fd0e4ef8a9eb64bcb3591f2a4f0b676d631bcf
MosesOdinaka <iriele.moses@gmail.com>
2023-08-17 17:19:10 -0700

    lists all shows contained in hbtn_0d_tvshows without a genre linked.

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- This query selects titles with no genre from the tv_shows table.
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
