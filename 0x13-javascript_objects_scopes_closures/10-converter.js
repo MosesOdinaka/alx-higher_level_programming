91f43539160716c86aa88601089d4616426595b0
MosesOdinaka <iriele.moses@gmail.com>
2023-11-15 03:22:18 +0100

    A class Rectangle.

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,5 @@
+#!/usr/bin/Node
+exports.converter = function (base) {
+    return num => num.toString(base);
+};
+  
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    return list.reduceRight(function (array, current) {
+      array.push(current);
+      return array;
+    },
+    []);
+};
+  
