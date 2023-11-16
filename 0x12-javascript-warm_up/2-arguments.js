64ce80c29f77360058aee624ebbccfd539e6485c
MosesOdinaka <iriele.moses@gmail.com>
2023-11-16 05:46:52 +0100

    Error fix.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-if (Process.argv.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
 }
-if (Process.argv.length === 3) {
+if (process.argv.length === 3) {
   console.log('Argument found');
 }
-if (Process.argv.length > 3) {
-  console.log('Argument found');
+if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
