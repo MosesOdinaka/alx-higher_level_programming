bcc385fa7447a1487a71def81d3e0d05fbb076ba
MosesOdinaka <iriele.moses@gmail.com>
2023-11-16 05:42:49 +0100

    Semistandard compliance fix.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (Process.argv.length === 2) {
-    console.log("No argument");
+  console.log('No argument');
 }
 if (Process.argv.length === 3) {
-    console.log("Argument found");
+  console.log('Argument found');
 }
 if (Process.argv.length > 3) {
-    console.log("Argument found");
+  console.log('Argument found');
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/urs/bin/node
 if (isNaN(parseInt(process.argv[2]))) {
-    console.log("Missing number of occurence");
-}else{
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-        console.log("C is fun");
-    }
+  console.log('Missing number of occurence');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
 }
