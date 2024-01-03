7ea221a470eb7b62e8f13b5c5582d61f9e2beafc
MosesOdinaka <iriele.moses@gmail.com>
2024-01-03 16:18:43 +0100

    Import the 'fs' module.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Import the 'fs' module
+const fileSystem = require('fs');
+
+// Define the file path and encoding
+const filePath = process.argv[2];
+const encodingType = 'utf-8';
+
+// Define the callback function
+function displayFileContent(err, fileContent) {
+  console.log(err || fileContent);
+}
+
+// Read the file
+fileSystem.readFile(filePath, encodingType, displayFileContent);
