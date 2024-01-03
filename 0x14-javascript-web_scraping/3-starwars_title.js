cdcaa866fd255015c151bd60d992c66e4bd8d11b
MosesOdinaka <iriele.moses@gmail.com>
2024-01-03 16:28:12 +0100

    Send a GET request to the URL and display the film title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Import the 'request' module
+const httpRequest = require('request');
+
+// Define the URL
+const filmUrl = 'http://swapi.co/api/films/' + process.argv[2];
+
+// Define the callback function
+function displayFilmTitle(error, httpResponse, responseBody) {
+  console.log(JSON.parse(responseBody).title || error);
+}
+
+// Send a GET request to the URL and display the film title
+httpRequest(filmUrl, displayFilmTitle);
