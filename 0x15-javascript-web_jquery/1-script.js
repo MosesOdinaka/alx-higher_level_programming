2c6b6b8ac73fce7fe5980cedaa79d95990f8fd74
MosesOdinaka <iriele.moses@gmail.com>
2024-01-10 19:29:46 +0100

    jQuery statement that changes the color.

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+// Select the HTML element with the tag name 'HEADER'
+const headerElement = $('HEADER');
+
+// Defines the color to ve applied
+const colorToApply = '#FF0000';
+
+// Applies the color to the selected element
+headerElement.css('color', colorToApply);
